26460ba18223f228367f81f1d479b59b55971b8e
david <david@buadze.com>
2025-08-24 20:20:04 +0000

    Update

--- ailbee-web-app/frontend/src/index.js
+++ ailbee-web-app/frontend/src/index.js
@@ -7,5 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <WrappedApp />
-  </React.StrictMode>
+  </React.StrictMode> // Triggering a deployment
 );
